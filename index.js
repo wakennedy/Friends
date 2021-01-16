babbdc4627805525d297b87bb736b9c3d4800899
wakennedy <alexkenned@gmail.com>
2021-01-16 14:36:41 -0800

    isolated each episode id. pre-mutate

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const fs = require("fs");
 
-let rawdata = fs.readFileSync("episodes.json");
-let episodes = JSON.parse(rawdata);
+let raw_master_list = fs.readFileSync("master_episode_list.json");
+let master_list = JSON.parse(raw_master_list);
+
+const mutateIndices = function (array) {
+  for (let i = 0; i <= array.length; i++) {
+    let item = array[i];
+    try {
+      console.log(`${i} - ${item.id}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
 console.log("App Start");
-console.log(episodes);
+mutateIndices(master_list);
+// console.log(master_list);
 console.log("App Close");
