59ed38ffa45a6c9c8081394086708416c6949a12
wakennedy <alexkenned@gmail.com>
2021-01-16 14:49:33 -0800

    pre_ex_1

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ let raw_master_list = fs.readFileSync("master_episode_list.json");
 let master_list = JSON.parse(raw_master_list);
 
 const mutateIndices = function (array) {
+  let newMaster = [];
   for (let i = 0; i <= array.length; i++) {
     let item = array[i];
+
     try {
-      console.log(`${i} - ${item.id}`);
+      item.id = i;
+      newMaster.push(item);
     } catch (err) {
       console.log(err);
     }
   }
+  return newMaster;
 };
 
 console.log("App Start");
-mutateIndices(master_list);
+// mutateIndices(master_list);
 // console.log(master_list);
+
+let newJson = JSON.stringify(mutateIndices(master_list));
+fs.writeFileSync("episodes.json", newJson);
+
 console.log("App Close");
