a6683d8f6aba40e134a67a3798a1f4d710568882
wakennedy <alexkenned@gmail.com>
2021-01-16 13:53:14 -0800

    node init & console logs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-import fs from "fs";
+const fs = require("fs");
+
+let rawdata = fs.readFileSync("episodes.json");
+let episodes = JSON.parse(rawdata);
+console.log("App Start");
+console.log(episodes);
+console.log("App Close");
