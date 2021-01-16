c901351ed9e95caeb10092e330cf596d018e4292
wakennedy <alexkenned@gmail.com>
2021-01-16 14:58:20 -0800

    post episode.json setup and test

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 
-let raw_master_list = fs.readFileSync("master_episode_list.json");
-let master_list = JSON.parse(raw_master_list);
+const raw_episodes_list = fs.readFileSync("episodes.json");
+const episodes_list = JSON.parse(raw_episodes_list);
+
+/////////////////////////   JSON SETUP
+////////////////////////////////////////////////////////////////////////////
+const raw_master_list = fs.readFileSync("master_episode_list.json");
+const master_list = JSON.parse(raw_master_list);
 
 const mutateIndices = function (array) {
   let newMaster = [];
@@ -17,12 +22,20 @@ const mutateIndices = function (array) {
   }
   return newMaster;
 };
+const epConfirm = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    const item = array[i];
+    console.log(`${item.id} - ${i}`);
+  }
+};
+///////////////////////////////////////////////////////////////////////////
 
 console.log("App Start");
-// mutateIndices(master_list);
-// console.log(master_list);
 
-let newJson = JSON.stringify(mutateIndices(master_list));
-fs.writeFileSync("episodes.json", newJson);
+///////////////////////////////// JSON SETUP
+// let newJson = JSON.stringify(mutateIndices(master_list));
+// fs.writeFileSync("episodes.json", newJson);
+// epConfirm(episodes_list);
+////////////////////////////////////////////////
 
 console.log("App Close");
