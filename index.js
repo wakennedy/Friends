9aa4b4f6a3695e893f92eade7e3a0810cd8a20be
wakennedy <alexkenned@gmail.com>
2021-01-24 17:40:29 -0800

    server get functional

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import express from "express";
-import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
 
-import usersRoutes from "./routes/users.js";
+// const usersRoutes = require("./routes/users.js");
 
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 
-app.use("/people", usersRoutes);
-app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+// app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Friends Episode API!"));
 app.all("*", (req, res) =>
   res.send("You've tried reaching a route that doesn't exist.")
 );
