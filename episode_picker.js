d85c0984e2d74f678ad44feb323f432c91ca8318
wakennedy <alexkenned@gmail.com>
2021-01-29 17:56:53 -0800

    original friends random/reset funcions all moved under input switch

--- episode_picker.js
+++ episode_picker.js
@@ -11,12 +11,45 @@ const search = fetchSetup.searchFetch;
 const input = function () {
   let done = false;
   while (!done) {
-    let sign = prompt("Please Enter Name ", "Guest");
-    console.log(`Hello ${sign}`);
-    let exit = prompt("Done?  ");
-    if (exit == "yes") {
-      done = true;
+    let sign = prompt("> ");
+    switch (sign) {
+      case "help":
+        console.log("Type a Command or 'done' to end.");
+        break;
+      case "test":
+        console.log("Test Case in input function");
+        break;
+      case "another":
+        console.log("Another Test Case");
+        break;
+      case "done":
+        done = true;
+        break;
+      case "reset":
+        console.log("Watched Episodes Reset");
+        console.log("------------------------------");
+        resetWorkingEpisodes();
+        break;
+      case "search":
+        search(betterArgs[1]);
+        break;
+      case "random":
+        let info = randomEpisode();
+        console.log(`S-Ep: ${info.season} - ${info.number}`);
+        console.log("------------------------------");
+        console.log(`${info.name}`);
+        console.log("------------------------------");
+        console.log(`${info.summary.replace(/<[^>]*>?/gm, "")}`);
+        console.log("------------------------------");
+        break;
+      default:
+        console.log("error");
     }
+    // console.log(`Hello ${sign}`);
+    // let exit = prompt("Done?  ");
+    // if (exit == "yes") {
+    //   done = true;
+    // }
   }
 };
 
@@ -28,23 +61,26 @@ console.log(`Args: ${args}`);
 console.log("------------------------------");
 
 switch (betterArgs[0]) {
-  case "reset":
-    console.log("Watched Episodes Reset");
-    console.log("------------------------------");
-    resetWorkingEpisodes();
+  case "input":
+    input();
     break;
-  case "search":
-    search(betterArgs[1]);
-    break;
-  case "random":
-    let info = randomEpisode();
-    console.log(`S-Ep: ${info.season} - ${info.number}`);
-    console.log("------------------------------");
-    console.log(`${info.name}`);
-    console.log("------------------------------");
-    console.log(`${info.summary.replace(/<[^>]*>?/gm, "")}`);
-    console.log("------------------------------");
-    break;
+  // case "reset":
+  //   console.log("Watched Episodes Reset");
+  //   console.log("------------------------------");
+  //   resetWorkingEpisodes();
+  //   break;
+  // case "search":
+  //   search(betterArgs[1]);
+  //   break;
+  // case "random":
+  //   let info = randomEpisode();
+  //   console.log(`S-Ep: ${info.season} - ${info.number}`);
+  //   console.log("------------------------------");
+  //   console.log(`${info.name}`);
+  //   console.log("------------------------------");
+  //   console.log(`${info.summary.replace(/<[^>]*>?/gm, "")}`);
+  //   console.log("------------------------------");
+  //   break;
   default:
     console.log("Sorry, that is not something I know how to do.");
 }
