d2f9a4c244b2fa854f382f52eb1723872cbfd659
wakennedy <alexkenned@gmail.com>
2021-02-18 12:15:59 -0800

    frontend init

---
+++ friends_front/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>{count}</p>
+  <button onClick={ () => setCount(count +1)}>
+
+
+  </button>
+  );
+}
+
+export default App;
