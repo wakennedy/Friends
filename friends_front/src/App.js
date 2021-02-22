8f8dada71ec6e9e2852a1ea20519900506e78339
wakennedy <alexkenned@gmail.com>
2021-02-22 08:20:49 -0800

    functional component update

--- friends_front/src/App.js
+++ friends_front/src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { Component, Fragment, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => setCount((prev) => prev + 1);
   return (
-    <p>{count}</p>
-  <button onClick={ () => setCount(count +1)}>
-
-
-  </button>
+    <Fragment>
+      <p>{count}</p>
+      <button onClick={handleClick}>Click Me</button>
+    </Fragment>
   );
 }
 
